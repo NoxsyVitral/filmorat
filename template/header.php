6e9250ffd9abe70afa390348ad4970f7825cc7ad
vold <vold@gmail.com>
2024-03-28 07:32:13 -0700

    navbar added

--- template/header.php
+++ template/header.php
@@ -6,4 +6,13 @@
     <title>Filmorat</title>
 </head>
 <body>
-    
+ <div class="nav">
+    <div class="left">
+        <h2>Filmorat</h2>
+    </div>
+    <div class="right">
+        <div class="search">
+            <h4>Search...</h4>
+        </div>
+    </div>
+ </div>
